328614fafb32c4eb8a98c3c212e3cdf42580eacf
ShiqiWangTony1 <shiqi.wang0728@gmail.com>
2024-05-30 21:00:47 -0400

    first commit

---
+++ Lesson_2_Magic.py
@@ -0,0 +1,31 @@
+# Task 1: Code Correction
+# number = input("Enter a number: ")
+
+# if number > 0:
+#     print("The number is positive.")
+# elif number = 0:
+#     print("The number is zero.")
+# else number < 0:
+#     print("The number is negative.")
+
+## Answer
+# number = float(input("Enter a number: "))
+
+# if number > 0:
+#     print("The number is positive.")
+# elif number == 0:
+#     print("The number is zero.")
+# else:
+#     print("The number is negative.")
+
+
+# Task 1: Identify the Greatest
+
+# num1 = float(input("Please enter the first number: "))
+# num2 = float(input("Please enter the second number: "))
+# num3 = float(input("Please enter the third number: "))
+
+# largest_num = max(num1,num2,num3)
+# print(f"The largest number is {largest_num}")
+
+
